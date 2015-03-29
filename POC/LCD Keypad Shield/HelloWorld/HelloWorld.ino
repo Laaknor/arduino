03094f53792c7a05646f8e9bd599f70b69db7dd0
Bjørn Gustav Baklid <bg@dyret.net>
2015-03-29 18:17:37 +0200

    Update HelloWorld.ino
    Added some info about the pin configuration for the LCD

--- POC/LCD Keypad Shield/HelloWorld/HelloWorld.ino
+++ POC/LCD Keypad Shield/HelloWorld/HelloWorld.ino
@@ -1,6 +1,13 @@
 // include the library code:
 #include <LiquidCrystal.h>
 
+//LCD RS pin to digital pin 8
+//LCD Enable pin to digital pin 9
+//LCD D4 pin to digital pin 4
+//LCD D5 pin to digital pin 5
+//LCD D6 pin to digital pin 6
+//LCD D7 pin to digital pin 7
+
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
