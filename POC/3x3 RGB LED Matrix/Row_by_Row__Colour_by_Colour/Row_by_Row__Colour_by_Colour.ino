5f40e5ff815f6640fb300aad3ac5f8a2f6ecd735
Bjørn Gustav Baklid <bg@dyret.net>
2015-04-05 22:13:57 +0200

    Commenting
    added some comments

--- POC/3x3 RGB LED Matrix/Row_by_Row__Colour_by_Colour/Row_by_Row__Colour_by_Colour.ino
+++ POC/3x3 RGB LED Matrix/Row_by_Row__Colour_by_Colour/Row_by_Row__Colour_by_Colour.ino
@@ -1,10 +1,10 @@
-int ledPins[] = {
-  4,5,6,7,8,9,10,11,12};
+int ledPins[] = {4,5,6,7,8,9,10,11,12}; //Array with output pins
 
 short delayTime = 100;
 
 void setup()
 {
+  //Set up pins to output, the easy way.
   int index;
   for (index = 0; index <= 8; index++)
   {
@@ -16,37 +16,43 @@ void setup()
 void loop()
 {
   int index;
-  for (index = 0; index <= 2; index++)
-  {
-    digitalWrite(ledPins[index], HIGH);
-    delay(delayTime);
-
-  }
-  for (index = 0; index <= 2; index++)
-  {
-    digitalWrite(ledPins[index], LOW);
-    delay(delayTime);
-
-  }
-  for (index = 3; index <= 5; index++)
-  {
-    digitalWrite(ledPins[index], HIGH);
-    delay(delayTime);
-
-  }
-  for (index = 3; index <= 5; index++)
-  {
-    digitalWrite(ledPins[index], LOW);
-    delay(delayTime);
-
-  }
   
+  //Light the rows red
+  for (index = 0; index <= 2; index++)
+  {
+    digitalWrite(ledPins[index], HIGH);
+    delay(delayTime);
+
+  }
+  //Turn them off again
+  for (index = 0; index <= 2; index++)
+  {
+    digitalWrite(ledPins[index], LOW);
+    delay(delayTime);
+
+  }
+  //Light the rows green
+  for (index = 3; index <= 5; index++)
+  {
+    digitalWrite(ledPins[index], HIGH);
+    delay(delayTime);
+
+  }
+  //Turn them off again
+  for (index = 3; index <= 5; index++)
+  {
+    digitalWrite(ledPins[index], LOW);
+    delay(delayTime);
+
+  }
+  //Light the rows blue
   for (index = 6; index <= 8; index++)
   {
     digitalWrite(ledPins[index], HIGH);
     delay(delayTime);
 
   }
+  //Turn them off again
   for (index = 6; index <= 8; index++)
   {
     digitalWrite(ledPins[index], LOW);
